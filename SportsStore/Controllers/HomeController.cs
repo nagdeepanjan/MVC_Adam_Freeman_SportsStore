ef57249533133bb035d9ebdf8811fc29e225a35f
nagdeepanjan <mail@deepanjannag.com>
2025-11-04 16:30:22 -0500

    Add HomeController tests, DI, pagination, and view updates
    Updated `HomeController` to use dependency injection for `IStoreRepository` and added pagination support in the `Index` action. Introduced a `PageSize` property to control the number of products displayed per page.
    
    Added a unit test `Can_Use_Repository` in `HomeControllerTests` to verify the controller's interaction with the repository. Mocking was used to simulate the repository, and assertions ensure correct product retrieval.
    
    Updated `Index.cshtml` to dynamically display product details using the model. Removed placeholder content and added a layout for product information.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IStoreRepository _repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repository)
         {
-            return View();
+            _repository = repository;
         }
+
+        public IActionResult Index(int productPage=1) => View(_repository.Products.OrderBy(p=>p.ProductID).Skip((productPage-1)*PageSize).Take(PageSize));
+        
     }
 }
