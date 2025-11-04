f3fbf4021d43718289ebbd154ac2d845c125d5e3
nagdeepanjan <mail@deepanjannag.com>
2025-11-04 14:11:34 -0500

    Add EF Core repository and SQL Server integration
    Introduced `EFStoreRepository` and `IStoreRepository` for data access abstraction. Updated `Product` class with additional properties and database column configuration. Added `StoreDbContext` for Entity Framework Core integration.
    
    Configured dependency injection for repository and DbContext in `Program.cs`. Enabled SQL Server support with connection string in `appsettings.json`. Added required NuGet packages and `dotnet-ef` CLI tool for migrations.
    
    Updated `SportsStore.csproj` to include EF Core dependencies.

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext _context;
+
+        public EFStoreRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();          //Repository Dependency Injection
+builder.Services.AddDbContext<StoreDbContext>((options => options.UseSqlServer(builder.Configuration.GetConnectionString("SportsStoreConnection"))));
+
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
 app.MapDefaultControllerRoute();
 
 app.Run();
